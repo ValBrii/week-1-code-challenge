b352dfed7d9fbb97c383a004e40cc969341fa75a
Valerie <britneyvalerie85@gmail.com>
2024-12-01 23:07:02 +0300

    comments added

--- challenge1.js
+++ challenge1.js
@@ -1,6 +1,6 @@
-function studentGrade() {
-    let marks;
-    if(marks < 0 || marks > 100 ) {
+function studentGrade(marks) {
+    
+    if(marks < 0 || marks > 100 ) { //If marks entered is less than 0 or greater than 100
         console.log('Invalid marks entered. Please insert a number between 0 and 100')
     } 
     else if(marks > 79) {
@@ -22,4 +22,4 @@ function studentGrade() {
 
 }
 
-studentGrade()
+studentGrade() // calling the function, insert the marks inside the brackets to  get the grade
